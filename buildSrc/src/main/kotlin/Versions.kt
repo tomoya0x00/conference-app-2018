6728882b8b380c9ea1b5aa5792e4426a981560f8
tomoya0x00 <tomoyann@gmail.com>
2022-02-23 13:52:47 +0900

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val compileSdk = 31
     const val minSdk = 23
     const val targetSdk = 30
-    const val gradleBuildTool = "7.1.0-rc01"
+    const val gradleBuildTool = "7.1.1"
     const val kotlin = "1.6.10"
     const val googleServices = "4.3.10"
     const val ktlint = "0.43.2"
@@ -10,9 +10,9 @@ object Versions {
     const val gradleVersions = "0.41.0"
     const val retrofit = "2.9.0"
     const val kotshi = "2.7.0"
-    const val lifecycle = "2.4.0"
+    const val lifecycle = "2.4.1"
     const val room = "2.4.1"
-    const val dagger = "2.40.5"
+    const val dagger = "2.41"
     const val kotpref = "2.13.1"
     const val glide = "4.12.0"
     const val groupie = "2.10.0"
